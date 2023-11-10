d02f2592467af05e1af4357e268adab8aa898253
fsuomins <fsuomins@student.42sp.org.br>
2023-11-10 10:07:59 -0300

    wip(ex00)

--- cpp/cpp00/ex00/megaphone.cpp
+++ cpp/cpp00/ex00/megaphone.cpp
@@ -6,7 +6,26 @@
 /*   By: fsuomins <fsuomins@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 18:57:36 by fsuomins          #+#    #+#             */
-/*   Updated: 2023/11/09 18:57:37 by fsuomins         ###   ########.fr       */
+/*   Updated: 2023/11/10 09:52:05 by fsuomins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <cstdlib>
+
+int	main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "shhhhh... I think the students are asleep... *";
+	else {
+		for (int i = 1; argv[i]; i++)
+        {
+			for (int j = 0; argv[i][j]; j++)
+				std::cout << (char) toupper(argv[i][j]);
+			if (i < argc - 1)
+				std::cout << ' ';
+		}
+	}
+	std::cout << std::endl;
+	return (EXIT_SUCCESS);
+}
